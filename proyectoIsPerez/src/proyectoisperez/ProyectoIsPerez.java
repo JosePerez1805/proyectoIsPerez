08ec99b53539b5a952ecc71e3305a73bbe1e0592
JosePerez1805 <jlpj1805ro@gmail.com>
2024-10-26 10:48:55 -0600

    Cambio1Perez
    Confirmacion Perez 1

---
+++ proyectoIsPerez/src/proyectoisperez/ProyectoIsPerez.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoisperez;
+
+/**
+ *
+ * @author jluis
+ */
+public class ProyectoIsPerez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Prubhe arepositorio 26/010/24");
+    }
+    
+}
